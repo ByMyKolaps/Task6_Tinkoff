1680513c33d427a3398a4f51d955a2b39c634314
ByMyKolaps <muradymov.bulat@mail.ru>
2023-03-04 19:44:51 +0300

    Initial

---
+++ Task6/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Task6
+{
+    public class Program
+    {
+        public static Tuple<int, int> Task6(long[] studentsHeights)
+        {
+            int i = -1, j = -1;
+            long iValue = 0;
+            bool isPossible = true;
+            for (int index = 0; index < studentsHeights.Length; index++)
+            {
+                // Если на i-ом месте все правильно, то двигаемся дальше
+                if (studentsHeights[index] % 2 == (index + 1) % 2)
+                    continue;
+                else
+                {
+
+                    // Если мы уже нашли пару элементов, которую нужно поменять,
+                    // то одной единственной перестановки не хватит и можно останавливать цикл
+                    if (i != -1 && j != -1)
+                    {
+                        isPossible = false;
+                        break;
+                    }
+                    if (i == -1)
+                    {
+                        i = index + 1;
+                        // Запоминаем, что за число стоит на неправильно позиции
+                        iValue = studentsHeights[index];
+                    }
+                    else
+                    {
+                        // Если текущее число, стоящее в неправильной позиции, противоположно по четности ранее найденному числу,
+                        // то мы нашли пару для перестановки
+                        if (iValue % 2 != studentsHeights[index] % 2)
+                            j = index + 1;
+                    }
+                }
+            }
+            if (i == -1 || j == -1 || i == j || !isPossible)
+            {
+                // Если последовательность правильная и ее длина больше 3-ех, то выводим 1 и 3
+                if (i == -1 && studentsHeights.Length >= 3)
+                {
+                    i = 1;
+                    j = 3;
+                }
+                else
+                    i = j = -1;
+
+            }
+            return Tuple.Create(i, j);
+        }
+
+        public static long[] GetNumericalInput()
+        {
+            var parametrs = Console.ReadLine().Split(' ').Select(s => long.Parse(s)).ToArray();
+            return parametrs;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = GetNumericalInput();
+                var result = Task6(input);
+                Console.WriteLine(result.Item1.ToString() + " " + result.Item2.ToString());
+            }
+        }
+    }
+}
---
+++ Task6_Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Task6;
+
+namespace Task6_Tests
+{
+    public class Tests
+    {
+        [TestCase(new long[] { 1, 2 }, -1, -1)]
+        [TestCase(new long[] { 2, 1 }, 1, 2)]
+        [TestCase(new long[] { 1, 1 }, -1, -1)]
+        [TestCase(new long[] { 2, 2 }, -1, -1)]
+        [TestCase(new long[] { 1, 2, 1 }, 1, 3)]
+        [TestCase(new long[] { 1, 2, 2 }, -1, -1)]
+        [TestCase(new long[] { 2, 1, 1 }, 1, 2)]
+        [TestCase(new long[] { 2, 1, 2 }, -1, -1)]
+        [TestCase(new long[] { 1, 1, 2 }, 2, 3)]
+        [TestCase(new long[] { 1, 1, 1 }, -1, -1)]
+        [TestCase(new long[] { 2, 2, 1 }, -1, -1)]
+        [TestCase(new long[] { 2, 2, 2 }, -1, -1)]
+        [TestCase(new long[] { 2, 1, 2, 1 }, -1, -1)]
+        [TestCase(new long[] { 1, 2, 1, 2 }, 1, 3)]
+        [TestCase(new long[] { 2, 1, 1, 2 }, 1, 2)]
+        [TestCase(new long[] { 1, 1, 2, 2 }, 2, 3)]
+        [TestCase(new long[] { 1, 2, 2, 1 }, 3, 4)]
+        [TestCase(new long[] { 2, 2, 1, 2 }, -1, -1)]
+        [TestCase(new long[] { 1, 1, 1, 2 }, -1, -1)]
+        [TestCase(new long[] { 1, 2, 1, 1 }, -1, -1)]
+        [TestCase(new long[] { 2, 1, 2, 2 }, -1, -1)]
+        public void Task6_Tests(long[] longs, int expectedI, int expectedJ)
+        {
+            var actual = Program.Task6(longs);
+            Assert.That(expectedI, Is.EqualTo(actual.Item1));
+            Assert.That(expectedJ, Is.EqualTo(actual.Item2));
+        }
+    }
+}
